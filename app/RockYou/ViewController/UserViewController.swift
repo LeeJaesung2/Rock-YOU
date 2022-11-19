c6dd54fda0f1bb66db004db16b84a40d7b18cc11
geun0196 <geun0196@naver.com>
2022-11-20 00:29:48 +0900

    FEAT : userInfoView and mapView backButton

--- app/RockYou/ViewController/UserViewController.swift
+++ app/RockYou/ViewController/UserViewController.swift
@@ -9,21 +9,118 @@ import UIKit
 
 class UserViewController: UIViewController {
 
+    
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nicknameView: UIView!
+    @IBOutlet weak var idView: UIView!
+    @IBOutlet weak var passwordView: UIView!
+    @IBOutlet weak var listView: UITextView!
+
+    var nickString = "nickname"
+    var idString = "id입니당"
+    var passwordString = "password입니당"
+    
+    
+    private lazy var nicknameLabel : UILabel = {
+        let label = UILabel()
+        label.text = self.nickString
+        label.font = .systemFont(ofSize: 17)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var idLabel : UILabel = {
+        let label = UILabel()
+        label.text = self.idString
+        label.font = .systemFont(ofSize: 17)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var passwordLabel : UILabel = {
+        let label = UILabel()
+        label.text = self.passwordString
+        label.font = .systemFont(ofSize: 17)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var idSetLabel : UILabel = {
+        let label = UILabel()
+        label.text = "아이디 : "
+        label.font = .boldSystemFont(ofSize: 15)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var passwordSetLabel : UILabel = {
+        let label = UILabel()
+        label.text = "비밀번호 : "
+        label.font = .boldSystemFont(ofSize: 15)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.viewInit()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func viewInit(){
+        mainView.layer.cornerRadius = 40
+        mainView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        imageView.layer.cornerRadius = imageView.layer.bounds.width / 2
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(systemName: "person.fill")
+        
+        nicknameView.layer.cornerRadius = 15
+        idView.layer.cornerRadius = 15
+        passwordView.layer.cornerRadius = 15
+        listView.layer.cornerRadius = 15
+        
+        addViewLabel()
     }
-    */
-
+   
+    private func addViewLabel(){
+        nicknameView.addSubview(nicknameLabel)
+        NSLayoutConstraint.activate([
+            self.nicknameLabel.centerYAnchor.constraint(equalTo: self.nicknameView.centerYAnchor),
+            self.nicknameLabel.leadingAnchor.constraint(equalTo: self.nicknameView.leadingAnchor, constant: 10),
+            self.nicknameLabel.trailingAnchor.constraint(equalTo: self.nicknameView.trailingAnchor, constant: 15),
+        ])
+        
+        idView.addSubview(idSetLabel)
+        NSLayoutConstraint.activate([
+            self.idSetLabel.centerYAnchor.constraint(equalTo: self.idView.centerYAnchor),
+            self.idSetLabel.leadingAnchor.constraint(equalTo: self.idView.leadingAnchor, constant: 10),
+        ])
+        
+        idView.addSubview(idLabel)
+        NSLayoutConstraint.activate([
+            self.idLabel.centerYAnchor.constraint(equalTo: self.idView.centerYAnchor),
+            self.idLabel.leadingAnchor.constraint(equalTo: self.idSetLabel.trailingAnchor, constant: 7),
+            self.idLabel.trailingAnchor.constraint(equalTo: self.idView.trailingAnchor, constant: 15),
+        ])
+        
+        passwordView.addSubview(passwordSetLabel)
+        NSLayoutConstraint.activate([
+            self.passwordSetLabel.centerYAnchor.constraint(equalTo: self.passwordView.centerYAnchor),
+            self.passwordSetLabel.leadingAnchor.constraint(equalTo: self.passwordView.leadingAnchor, constant: 10),
+        ])
+        
+        passwordView.addSubview(passwordLabel)
+        NSLayoutConstraint.activate([
+            self.passwordLabel.centerYAnchor.constraint(equalTo: self.passwordView.centerYAnchor),
+            self.passwordLabel.leadingAnchor.constraint(equalTo: self.passwordSetLabel.trailingAnchor, constant: 7),
+            self.passwordLabel.trailingAnchor.constraint(equalTo: self.passwordView.trailingAnchor, constant: 15),
+        ])
+    }
+    
+    private func addBicycleList(){
+        
+    }
 }
